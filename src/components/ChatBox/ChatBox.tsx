3d9f492b2c1c1d30f56b491eb332649dec5a5b52
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-02-22 14:35:39 -0500

    Update image

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import './ChatBox.css';
 import user from '../../assets/user.png';
-import avatar from '../../assets/avatar.png';
+import avatar from '../../assets/bot.png';
 import { ChatType } from '../../interfaces/interfaces';
 
 interface ChatBoxProps {
@@ -12,11 +12,14 @@ const ChatBox:FC<ChatBoxProps> = (props) => {
 
     return (
     <div className={props.type == ChatType.BOT ? 'chatbox chatbox--bot':'chatbox'}>
-        <img src={props.type === ChatType.BOT ? avatar : user}
-            className='chatbox__image' alt='User Icon'
-        />
+        <div className='chatbot__text-container'>
+            <img src={props.type === ChatType.BOT ? avatar : user}
+                className='chatbox__image' alt='User Icon'
+            />
 
-        <p>Lorem ipsum puta madre</p>
+            <p>Lorem ipsum puta madre</p>
+        </div>
+        
     </div>)
 }
 
